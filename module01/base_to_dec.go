b579c518cc132e0c589908cdd4a1d9f6c4fb0dfa
Joe Saraceno <honestcomrade@gmail.com>
2024-03-09 08:33:08 -0700

    basetodec

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -10,15 +12,11 @@ package module01
 func BaseToDec(value string, base int) int {
 	res := 0
 	multiplier := 1
-	// var num int
 	for i := len(value) - 1; i >= 0; i-- {
-
+		var val int
+		fmt.Sscanf(string(value[i]), "%X", &val)
+		res += multiplier * val
+		multiplier *= base
 	}
-	// for _, char := range value {
-	// 	exp := len(value) - 1
-	// 	num = num*10 + int(char-'0')
-	// 	x := num*base ^ exp
-	// 	sum += x
-	// }
 	return res
 }
